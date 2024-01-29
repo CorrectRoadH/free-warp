7f2d1edf36f86c567b522b804898d2f14ecb08ae
CorrectRoadH <a778917369@gmail.com>
2024-01-29 09:48:12 +0800

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,16 +21,14 @@ export default function RootLayout({
       <head>
         <Script
           id="google-analytics"
+          src="https://www.googletagmanager.com/gtag/js?id=G-6X034D5RZB"
           dangerouslySetInnerHTML={{
             '__html': `
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-6X034D5RZB"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-6X034D5RZB');
-            </script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-6X034D5RZB');
             `}} 
           />
         </head>
