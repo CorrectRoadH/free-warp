f92dd9c1b2d9df609077700394cd0c117240b509
CorrectRoadH <a778917369@gmail.com>
2024-01-29 13:08:42 +0800

    feat: add mixplane

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
       <head>
         <Script
           id="google-analytics"
+          strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-6X034D5RZB"
           dangerouslySetInnerHTML={{
             '__html': `
@@ -30,7 +31,34 @@ export default function RootLayout({
           
             gtag('config', 'G-6X034D5RZB');
             `}} 
-          />
+          ></Script>
+
+        <Script id="script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html:`
+            (function(f,b){if(!b.__SV){var e,g,i,h;window.mixpanel=b;b._i=[];b.init=function(e,f,c){function g(a,d){var b=d.split(".");2==b.length&&(a=a[b[0]],d=b[1]);a[d]=function(){a.push([d].concat(Array.prototype.slice.call(arguments,0)))}}var a=b;"undefined"!==typeof c?a=b[c]=[]:c="mixpanel";a.people=a.people||[];a.toString=function(a){var d="mixpanel";"mixpanel"!==c&&(d+="."+c);a||(d+=" (stub)");return d};a.people.toString=function(){return a.toString(1)+".people (stub)"};i="disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");
+            for(h=0;h<i.length;h++)g(a,i[h]);var j="set set_once union unset remove delete".split(" ");a.get_group=function(){function b(c){d[c]=function(){call2_args=arguments;call2=[c].concat(Array.prototype.slice.call(call2_args,0));a.push([e,call2])}}for(var d={},e=["get_group"].concat(Array.prototype.slice.call(arguments,0)),c=0;c<j.length;c++)b(j[c]);return d};b._i.push([e,f,c])};b.__SV=1.2;e=f.createElement("script");e.type="text/javascript";e.async=!0;e.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?
+            MIXPANEL_CUSTOM_LIB_URL:"file:"===f.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\\/\\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";g=f.getElementsByTagName("script")[0];g.parentNode.insertBefore(e,g)}})(document,window.mixpanel||[]);
+
+              const randomId = ()=>{
+                //generate a random id
+                const id = Math.random().toString(36).substr(2, 9);
+                localStorage.setItem('mixpanel-identify',id)
+                return id
+              }
+              // get id from storage 
+              const id = localStorage.getItem('mixpanel-identify')
+              if(!id){
+                id = randomId()
+              }
+
+              mixpanel.init('b1c5974ab66de18afaacc71fe0ff436c', {track_pageview: true, persistence: 'localStorage'});
+              mixpanel.identify(id);
+            `
+          }}
+        ></Script>
+
         </head>
         <body className={inter.className}>
         <Toaster />
